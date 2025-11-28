f42af961a3a20b8a28c582abf1dd8dcb029d02dd
Zubair <zubairahmadzai@cmail.carleton.ca>
2025-11-28 09:45:34 -0500

    Construct code for greeting

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -9,9 +9,15 @@
 
 // Add your code right below, good luck!
 
+const firstName = "Carlos";
+const lastName = "Stevenson";
+const thisYear = 1965;
+const birthYear = 1947;
 
+const fullName = firstName + " " + lastName;
+const age = thisYear - birthYear;
 
-
+const greeting = "Hello! My name is " + fullName + " and I am " + age + " years old.";
 
 // Do not change this
 module.exports = {
