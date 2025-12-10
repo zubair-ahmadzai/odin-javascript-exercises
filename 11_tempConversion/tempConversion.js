0ff881620aeaba40b4fef6b7336a2c2df97b3933
Zubair <zubairahmadzai@cmail.carleton.ca>
2025-12-10 13:47:34 -0500

    Write conversion functions and rounding function

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(value) {
+  return roundToOneDecimal((value - 32) * (5/9));
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(value) {
+  return roundToOneDecimal((value * (9/5) + 32));
 };
+
+const roundToOneDecimal = function(value) {
+  return Math.round(value * 10) / 10;
+}
 
 // Do not edit below this line
 module.exports = {
