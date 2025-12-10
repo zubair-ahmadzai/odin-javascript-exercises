d7f4da6e712acdbb7cc0f3b39e6fea6425bab10f
Zubair <zubairahmadzai@cmail.carleton.ca>
2025-12-10 13:19:06 -0500

    Write leapYears() function

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,6 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 100 === 0) return year % 400 === 0;
+    return year % 4 === 0;
 };
 
 // Do not edit below this line
