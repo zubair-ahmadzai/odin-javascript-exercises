8eabbc03555e80aea0e5b5be8bfe105abe308162
Zubair <zubairahmadzai@cmail.carleton.ca>
2025-12-27 19:59:06 -0500

    Complete exercise "12_calculator"

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce(((sum, current) => sum + current), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce(((sum, current) => sum * current), 1);
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	return base ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num == 0) { return 1 };
+  let result = num;
+  for (let i = num-1; i > 0; i--) {
+    result *= i;
+  } return result;
 };
 
 // Do not edit below this line
