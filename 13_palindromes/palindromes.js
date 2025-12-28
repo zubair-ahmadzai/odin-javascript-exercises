6e71b7904b107de5e66da2fbda1a03972a08b8c3
Zubair <zubairahmadzai@cmail.carleton.ca>
2025-12-27 20:32:33 -0500

    Complete exercise "13_palindromes"

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphaNumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
+    
+    const cleanedString = string
+        .toLowerCase()
+        .split("")
+        .filter((char) => alphaNumerical.includes(char))
+        .join("");
 
+    const reversedString = cleanedString.split("").reverse().join("");
+
+    return cleanedString === reversedString;
 };
 
 // Do not edit below this line
