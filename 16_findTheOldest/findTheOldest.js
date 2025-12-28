fd95892b9ac3f49854a7d9d87d7798ca155bab22
Zubair <zubairahmadzai@cmail.carleton.ca>
2025-12-27 23:32:25 -0500

    Complete exercise "16_findTheOldest"

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const getAge = function (birth, death) {
+    if (!death) {
+        death = (new Date()).getFullYear();
+    } return death - birth;
+};
 
+const findTheOldest = function(people) {
+    return people.reduce((oldest, current) => {
+        let oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        let currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+        return (currentAge > oldestAge ? current : oldest);
+    });
 };
 
 // Do not edit below this line
