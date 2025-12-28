c3edfcaf2523bf15c7a48afec056ade9b6d6528f
Zubair <zubairahmadzai@cmail.carleton.ca>
2025-12-27 22:48:29 -0500

    Complete exercise "14_fibonacci"

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(indexArg) {
+    let index;
+    if (typeof indexArg !== "number") {
+        index = parseInt(indexArg);
+    } else {
+        index = indexArg;
+    }
+    
+    if (index < 0) { return "OOPS" };
+    if (index == 0) { return 0 };
 
+    let prevPrev = 0;
+    let prev = 1;
+    let current;
+    for (let i = 2; i <= index; i++) {
+        current = prevPrev + prev;
+        prevPrev = prev;
+        prev = current;
+    } return prev;
 };
 
 // Do not edit below this line
